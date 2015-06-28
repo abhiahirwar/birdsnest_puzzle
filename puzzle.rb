a872dbefbab5539b1fc40397042b5e06b9faccab
Abhishek Ahirwar <abhiahirwar@gmail.com>
2015-06-29 00:09:27 +1000

    check_number method created

--- puzzle.rb
+++ puzzle.rb
@@ -1,2 +1,15 @@
 module Puzzle
+  def self.check_number(number)
+    if (((number % 4) == 0) && ((number % 5) == 0))
+      "oi ay oi"                          #return multiple of 5 and 4 => 'oi ay oi'
+    elsif (((number % 3) == 0) && ((number % 5) == 0))
+      "oi ay"                             #return multiple of 5 and 3 => 'oi ay'
+    elsif ((number % 4) == 0)
+      "oi"                                #return multiple of 4 => 'oi'
+    elsif ((number %  5) == 0)
+       "ay"                               #return multiple of 5 => 'ay'
+    else
+       number.to_s                        #return original number
+    end
+  end
 end
